04bad951ae11bc756834a028d92bc8eaf1a309bd
PawanRajesh <128175952+pawanrajesh@users.noreply.github.com>
2023-03-18 23:48:17 +0000

    made a random game

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 name = input("Enter your name: ")
 print("My Name is: ",name)
 print(len(name))
+
+favoriteVideoGame= input("enter video game")
+print("my favorite video game is:",favoriteVideoGame)
+print (len(favoriteVideoGame))
